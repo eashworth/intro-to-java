200d2fe3b00916141130f506dcea8099002930b1
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2019-11-14 00:20:46 +0000

    Add class Main, showing variables of various data types

---
+++ IntroToJava/src/introtojava/Main.java
@@ -0,0 +1,21 @@
+package introtojava;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// primitive data types:
+		int hello_world = 9;
+		double num2 = 5.0;
+		boolean b = false;
+		char c = 'h';
+		// not primitive:
+		String str = "9";
+		
+		int hello = hello_world;
+		
+		System.out.println(hello);
+
+	}
+
+}
---
+++ IntroToJava/src/module-info.java
@@ -0,0 +1,2 @@
+module introtojava {
+}
